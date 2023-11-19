845fa9e102b811354f16421bd62e8187cae36a5a
ALIAS Antoine <alias.antoine@orange.fr>
2023-11-19 15:43:08 +0100

    [SETUP] Add setup code + [REQUIREMENTS] Small changes

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as file:
+    requirements = file.read().splitlines()
+
+
+setup(
+    name="First containerized API",
+    version="1.0.0",
+    author="alias",
+    author_email="alias@fake_mail.com",
+    description="Fake API",
+    include_data_package=True,
+    packages=find_packages(include=["api", "api.*"]),
+    python_requires=">=3.10",
+    install_requires=requirements
+)
