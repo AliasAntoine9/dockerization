6aa01f8792d47855b0a6d7286001b1db353c3964
ALIAS Antoine <alias.antoine@orange.fr>
2023-11-19 15:22:10 +0100

    Add new endpoint => cars/s2000

--- api/controller.py
+++ api/controller.py
@@ -6,3 +6,17 @@ generic_controller = APIRouter()
 @generic_controller.get("/")
 def read_root():
     return "Hello World"
+
+
+@generic_controller.get("/cars/s2000")
+def get_s2000_info():
+    return {
+        "specification": {
+            "Engine name": "VTEC",
+            "Cubic capacity": "2.0L",
+            "Horse power": 240,
+            "Production year": "1999 - 2009",
+            "Type": "Convertible",
+            "Price in dollars": 35000
+        }
+    }
